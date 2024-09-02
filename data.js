eb2cd5c97a5dbaa46a4faa76343cbe99b3cd3495
Omar <omar.hatim.sakr@gmail.com>
2024-09-02 22:15:13 +0300

    new 3d

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-istockphoto-1298990449-1024x1024",
+      "name": "istockphoto-1298990449-1024x1024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.713869752339157,
+          "pitch": 0.1895923119238443,
+          "rotation": 0.7853981633974483,
+          "target": "0-istockphoto-1298990449-1024x1024"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-240_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg",
+      "name": "240_F_322882600_y6JbONLD7YLdRrU5LFQReuq8YUwasfgg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 120,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9101003299648234,
+          "pitch": 0.17858959845506028,
+          "rotation": 0,
+          "target": "1-240_f_322882600_y6jbonld7yldrru5lfqreuq8yuwasfgg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-shot-panoramic-composition-library",
+      "name": "shot-panoramic-composition-library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.108131951673812,
+          "pitch": 0.5807169449425817,
+          "rotation": 0,
+          "target": "0-istockphoto-1298990449-1024x1024"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
